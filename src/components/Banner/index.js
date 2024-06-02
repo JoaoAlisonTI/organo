743d09be3bb7631bc03aeec4ec82779b19437d41
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-06-02 16:09:34 -0300

    feat: adding the Banner component

---
+++ src/components/Banner/index.js
@@ -0,0 +1,9 @@
+import "./Banner.css"
+
+export const Banner = () => {
+  return (
+  <header className="banner">
+    <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+  </header>
+  )
+}
