509d05f0c356eb28d8c061b525e0e5ca8b9f0608
JoaoAlisonTI <joaoalison.ti@gmail.com>
2024-06-18 14:05:45 -0300

    feat: adding Collaborator component

---
+++ src/components/Collaborator/index.js
@@ -0,0 +1,15 @@
+import './Collaborator.css'
+
+export const Collaborator = ({ nome, imagem, cargo }) => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src={imagem} alt={nome}/>
+      </div>
+      <div className='rodape'>
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
+      </div>
+    </div>
+  )
+}
